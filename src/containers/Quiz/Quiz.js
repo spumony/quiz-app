d193454ba568f414d250c17887846eda8c99d9aa
Chumak Alexandr <ciumac.dev@gmail.com>
2020-06-22 12:54:06 +0300

    test message

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -20,7 +20,7 @@ class Quiz extends Component {
     return (
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
-          <h1>Ответьте на все вопросы***</h1>
+          <h1>Ответьте на все вопросы*****</h1>
 
           {
             this.props.loading || !this.props.quiz
